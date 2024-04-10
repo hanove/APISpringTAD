940c1141fdb274f9885375e39cfa0ee5fe2bc474
Heitor Aparecido <heitor.ap.silva98@gmail.com>
2024-04-10 20:27:17 -0300

    Initial commit

---
+++ src/test/java/com/example/apispringtad/ApiSpringTadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apispringtad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSpringTadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
