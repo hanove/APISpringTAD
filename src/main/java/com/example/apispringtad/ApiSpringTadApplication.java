b8467df8d095cfe8946f7872cbd0dfffb577b810
lucas-marchi <lucasmarchi20@gmail.com>
2024-04-10 21:36:26 -0300

    ok

--- src/main/java/com/example/apispringtad/ApiSpringTadApplication.java
+++ src/main/java/com/example/apispringtad/ApiSpringTadApplication.java
@@ -2,6 +2,7 @@ package com.example.apispringtad;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//teste
 
 @SpringBootApplication
 public class ApiSpringTadApplication {
