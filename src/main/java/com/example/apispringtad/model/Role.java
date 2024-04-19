61b22b221b0ba69103c80b0b6e400d9e643116a8
Heitor Aparecido <heitor.ap.silva98@gmail.com>
2024-04-18 23:17:57 -0300

    Auth added

--- src/main/java/com/example/apispringtad/model/Role.java
+++ src/main/java/com/example/apispringtad/model/Role.java
@@ -4,21 +4,21 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
 @Entity
-public class Atuador {
-
+@Table(name = "roles")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class Role {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String nome;
-    private boolean status;
+    private RoleName name;
 }
